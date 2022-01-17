e35085ee489b297a1ad89e9000f799750c75fd50
EC2 Default User <ec2-user@ip-192-168-4-49.us-east-2.compute.internal>
2022-01-17 14:36:25 +0000

    added config for flux

--- frontend/load.sh
+++
@@ -1,4 +0,0 @@
-while true;
-do
-    curl http://k8s-default-ingressf-87236c3d2c-1932050298.us-east-2.elb.amazonaws.com/
-done
---
+++ load.sh
@@ -0,0 +1,4 @@
+while true;
+do
+    curl http://k8s-default-ingressf-87236c3d2c-1248513930.us-east-2.elb.amazonaws.com/
+done
